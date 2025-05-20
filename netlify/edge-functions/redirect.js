cd573bb0f1a3a465e77ae53ccb165b700e350df7
Ashutosh <computerlogic25@gmail.com>
2025-05-20 10:24:33 +0530

    edge function update

--- netlify/edge-functions/redirect.js
+++ netlify/edge-functions/redirect.js
@@ -3,7 +3,17 @@ export default async function handler(request) {
   console.log("url", url);
   const path = url.pathname;
   console.log("path", path);
-
+  if (
+    url.search &&
+    (url.search.includes("teamId=") || url.search.includes("userId="))
+  ) {
+    return new Response("", {
+      status: 404,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
   if (!path.endsWith("/")) {
     url.pathname = `${path}/`; // Add trailing slash
     return Response.redirect(url.toString(), 301); // Permanent redirect
